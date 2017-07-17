c5618e7aa9839dcb044ac5f3449986408b7915d7
Randy Ferrer <randfer@gmail.com>
2017-07-16 20:33:36 -0400

    Basic UI rendering using Google books API JSON format

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import Parser from 'html-react-parser';
+import data from '../../data.json';
 
 function App() {
   return (
-    <div>
+    <div className="row">
       <h1>Book List</h1>
+      {data.items.map(book =>
+        <div key={book.id}>
+          <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="" />
+          <h3>
+            {book.volumeInfo.title} :&nbsp;
+            <span>{book.volumeInfo.subtitle}</span>
+          </h3>
+          <ul>
+            {book.volumeInfo.authors.map(author =>
+              <li key={author}>
+                {author}
+              </li>
+            )}
+          </ul>
+          <p>
+            {Parser(book.searchInfo.textSnippet)}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
